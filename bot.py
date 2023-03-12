c081047911035e441463752fe06823c43d4143fc
Martin Vidjeskog <martin.vidjeskog@hotmail.com>
2023-03-12 17:02:11 +0200

    process chat messages and only allow adming to interact with bot

--- bot.py
+++ bot.py
@@ -1,15 +1,49 @@
 import discord
 
-class Bot:
 
-    def __init__(self, token: str):
+class Bot:
+    """ Simple Discord bot class.
+    """
+
+    def __init__(self, token: str, admin: str, app_id: str):
         self.TOKEN = token
+        self.ADMIN = admin
+        self.APPLICATION_ID = app_id
+
+    def should_message_be_processed(self, message, client):
+        """ Takes discord message as input, and checks whether it should be processed.
+            Returns True if the message should be processed.
+        """
+        # prevent loops by filtering messages from bot
+        if message.author == client.user:
+            return False
+        # only allow admin to interact with the bot
+        if str(message.author) != self.ADMIN:
+            return False
+        # only process messages where the first mention is the bot
+        if len(message.mentions) == 0 or str(message.mentions[0].id) != self.APPLICATION_ID:
+            return False
+        return True
 
     def run_discord_bot(self):
-        client = discord.Client(intents=discord.Intents.default())
+        """ Set event handling and run the discord bot.
+        """
+        client = discord.Client(intents=discord.Intents.all())
 
         @client.event
         async def on_ready():
             print(f"{client.user} is now running")
-        
+
+        @client.event
+        async def on_message(message):
+            if not self.should_message_be_processed(message, client):
+                return
+
+            username = str(message.author)
+            user_message = str(message.content)
+            channel = str(message.channel)
+            message_is_from_admin = username == self.ADMIN
+
+            print(username, user_message, channel, message_is_from_admin)
+
         client.run(self.TOKEN)
--- main.py
+++ main.py
@@ -3,12 +3,18 @@ from dotenv import dotenv_values
 
 ENV_VAR_FILENAME = ".env"
 ENV_VAR_TOKEN = "BOT_TOKEN"
+ENV_VAR_ADMIN = "ADMIN_USERNAME"
+ENV_VAR_BOT_APP_ID = "BOT_APPLICATION_ID"
 
 def check_configuration(config: dict):
     """ Check that all required environmental variables are present in the configuration.
         Throws an error if any of the variables is missing.
     """
-    required_variables = [ENV_VAR_TOKEN]
+    required_variables = [
+        ENV_VAR_TOKEN, 
+        ENV_VAR_ADMIN,
+        ENV_VAR_BOT_APP_ID
+    ]
     for variable_name in required_variables:
         if variable_name not in config.keys():
             raise Exception(f"Environmental variable {variable_name} is not defined!")
@@ -21,5 +27,8 @@ if __name__ == "__main__":
     check_configuration(config)
 
     token = config[ENV_VAR_TOKEN]
+    admin = config[ENV_VAR_ADMIN]
+    app_id = config[ENV_VAR_BOT_APP_ID]
 
-    Bot(token=token).run_discord_bot()
+
+    Bot(token=token, admin=admin, app_id=app_id).run_discord_bot()
