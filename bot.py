924b6c2413e176b5a1ad39f6c9c052b5cf6d2bcd
Martin Vidjeskog <martin.vidjeskog@hotmail.com>
2023-03-12 13:08:00 +0200

    connect bot to channel

--- bot.py
+++ bot.py
@@ -1,8 +1,15 @@
+import discord
 
 class Bot:
 
-    def __init__(self):
-        pass
+    def __init__(self, token: str):
+        self.TOKEN = token
 
     def run_discord_bot(self):
-        pass
+        client = discord.Client(intents=discord.Intents.default())
+
+        @client.event
+        async def on_ready():
+            print(f"{client.user} is now running")
+        
+        client.run(self.TOKEN)
--- main.py
+++ main.py
@@ -1,6 +1,25 @@
 from bot import Bot
+from dotenv import dotenv_values
+
+ENV_VAR_FILENAME = ".env"
+ENV_VAR_TOKEN = "BOT_TOKEN"
+
+def check_configuration(config: dict):
+    """ Check that all required environmental variables are present in the configuration.
+        Throws an error if any of the variables is missing.
+    """
+    required_variables = [ENV_VAR_TOKEN]
+    for variable_name in required_variables:
+        if variable_name not in config.keys():
+            raise Exception(f"Environmental variable {variable_name} is not defined!")
 
 if __name__ == "__main__":
-    # run the bot
-    discord_bot = Bot()
-    discord_bot.run_discord_bot()
+    # load environmental variables
+    config = dotenv_values(ENV_VAR_FILENAME)
+    
+    # check that configuratio is valid
+    check_configuration(config)
+
+    token = config[ENV_VAR_TOKEN]
+
+    Bot(token=token).run_discord_bot()
