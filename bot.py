8fc5b0c0fe9cbd94ea7a5446001555f47ad67852
Martin Vidjeskog <martin.vidjeskog@hotmail.com>
2023-03-12 12:04:31 +0200

    project sceleton

---
+++ bot.py
@@ -0,0 +1,8 @@
+
+class Bot:
+
+    def __init__(self):
+        pass
+
+    def run_discord_bot(self):
+        pass
---
+++ main.py
@@ -0,0 +1,6 @@
+from bot import Bot
+
+if __name__ == "__main__":
+    # run the bot
+    discord_bot = Bot()
+    discord_bot.run_discord_bot()
